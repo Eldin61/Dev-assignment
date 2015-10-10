438d4dc24f90972bf781925de06fbf1d7605a3c1
Eldin61 <eldin.331@gmail.com>
2015-10-10 12:44:45 +0200

    dashboard

---
+++ src/dev/assignment/dashboard.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dev.assignment;
+
+import static dev.assignment.DevAssignment.primaryStage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.eclipse.persistence.internal.libraries.asm.util.ASMifierAnnotationVisitor;
+
+/**
+ *
+ * @author eldin
+ */
+public class dashboard extends Application{;
+    
+    Stage stage;
+    Parent root;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+        root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+        
+    }
+    
+}
